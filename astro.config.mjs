913bb4f2abcd44cb2ec08e364cf8419012d2c100
Christian Hall <christianhall@Christians-MacBook-Air.local>
2026-04-28 15:13:16 +0100

    Remove sitemap integration to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://christianhall.site',
@@ -10,6 +9,5 @@ export default defineConfig({
     mdx(),
     tailwind({ applyBaseStyles: false }),
     react(),
-    sitemap(),
   ],
 });
